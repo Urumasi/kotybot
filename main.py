0f37b5a98fae3b0cd9ea301faa14de268db6ad95
Urumasi <Urumasi@email.cz>
2019-11-26 14:55:10 +0100

    Fix indents, save info loggin to file

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from client import Bot
 from config import Config
 
 if __name__ == "__main__":
-	try:
-		logging.info("Creating bot")
-		bot = Bot(Config.sound_path)
-		logging.info("Starting bot")
-		bot.run(Config.key)
-	except Exception as e:
-		logging.error(str(e))
+    try:
+        logging.basicConfig(filename='all.log',level=logging.INFO)
+        logging.info("Creating bot")
+        bot = Bot(Config.sound_path)
+        logging.info("Starting bot")
+        bot.run(Config.key)
+    except Exception as e:
+        logging.error(str(e))
 
